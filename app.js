bf74856c71f2b2687fd4d62f80ff8e26671d95c6
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-09-26 22:35:48 -0400

    Users can create new channels and join

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const { engine } = require('express-handlebars');
 
 // Socket.io
 const io = require('socket.io')(server);
-//We'll store our online users here
 let onlineUsers = {};
+//Save the channels in this object.
+let channels = { "General": [] };
+
 io.on("connection", (socket) => {
-    // Make sure to send the users to our chat file
-    require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+    // Make sure to send the channels to our chat file
+    require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
 
 // Express-Handlebars
 app.engine('handlebars', engine({ defaultLayout: 'main' })); // Assuming you have a main.handlebars
